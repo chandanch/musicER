0ad0583e73da1dbba619ef2f2174dc58a4ee59d5
chandanch <chandan.cec107@gmail.com>
2017-12-05 19:52:06 +0530

    catch all route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { SearchComponent } from './search/search.component';
 import { HeaderComponent } from './header/header.component';
+import { DefaultComponent } from './default/default.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent, pathMatch: "full"},
   {path: 'search', component: SearchComponent},
   {path: 'find', redirectTo: 'search'},
-  {path: 'home', redirectTo: ''}
+  {path: 'home', redirectTo: ''},
+  {path: '**', component: DefaultComponent}
 ]
 
 @NgModule({
@@ -20,6 +22,7 @@ const routes: Routes = [
     HomeComponent,
     SearchComponent,
     HeaderComponent,
+    DefaultComponent,
   ],
   imports: [
     BrowserModule,
