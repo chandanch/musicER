ab22da0d053b6756e9b5cb2314a1b8c0de3725ff
chandanch <chandan.cec107@gmail.com>
2017-12-05 19:04:12 +0530

    route configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router'
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './search/search.component';
+import { HeaderComponent } from './header/header.component';
 
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: '/search', component: SearchComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    HeaderComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
